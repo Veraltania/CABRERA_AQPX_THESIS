da9b4c8f9a6cb4b59bb88565ec85b503eaea8548
Veralta5630 <neo.elrond.cabrera@gmail.com>
2026-04-06 20:53:33 +0900

    fixed glitch in pso_sweep_phi

--- Evolutionary_Algorithm_Testing/pso/pso_sweep_phi.py
+++ Evolutionary_Algorithm_Testing/pso/pso_sweep_phi.py
@@ -2,7 +2,7 @@ import os
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-from tqdm import tqdm  # Added tqdm import
+from tqdm import tqdm
 from Evolutionary_Algorithm_Testing.pso.pso_optimizer import PSOOptimizer
 
 if __name__ == "__main__":
@@ -10,140 +10,108 @@ if __name__ == "__main__":
     # Dynamically grab the directory where this specific python script lives
     SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-    transfer_functions_to_run = [
-        {
-            "base_dir": "results_pso_sweep_phi_do_feb5_daytime",
-            "tf_params": {
-                'tf_num': [1.346],
-                'tf_den': [1551.955, 1],
-                'tf_delay': 0.0,
-                'tf_n_pade': 2,
-                'computed_delay': 0.05,
-                'is_reverse_acting': False,
-                'max_kp': 100.0
-            }
+    # Refactored from a list into a single dictionary so .keys() and .items() work natively
+    transfer_functions = {
+        "pso_sweep_phi_do_feb5_daytime": {
+            'tf_num': [1.346],
+            'tf_den': [1551.955, 1],
+            'tf_delay': 0.0,
+            'tf_n_pade': 2,
+            'computed_delay': 0.05,
+            'is_reverse_acting': False,
+            'max_kp': 100.0
         },
-        {
-            "base_dir": "results_pso_sweep_phi_do_feb7_daytime",
-            "tf_params": {
-                'tf_num': [1.133],
-                'tf_den': [2833.82, 1],
-                'tf_delay': 0.0,
-                'tf_n_pade': 2,
-                'computed_delay': 0.05,
-                'is_reverse_acting': False,
-                'max_kp': 100.0
-            }
+        "pso_sweep_phi_do_feb7_daytime": {
+            'tf_num': [1.133],
+            'tf_den': [2833.82, 1],
+            'tf_delay': 0.0,
+            'tf_n_pade': 2,
+            'computed_delay': 0.05,
+            'is_reverse_acting': False,
+            'max_kp': 100.0
         },
-        {
-            "base_dir": "results_pso_sweep_phi_do_feb25_daytime",
-            "tf_params": {
-                'tf_num': [2.287],
-                'tf_den': [3010.296, 1],
-                'tf_delay': 0.0,
-                'tf_n_pade': 2,
-                'computed_delay': 0.05,
-                'is_reverse_acting': False,
-                'max_kp': 100.0
-            }
+        "pso_sweep_phi_do_feb25_daytime": {
+            'tf_num': [2.287],
+            'tf_den': [3010.296, 1],
+            'tf_delay': 0.0,
+            'tf_n_pade': 2,
+            'computed_delay': 0.05,
+            'is_reverse_acting': False,
+            'max_kp': 100.0
         },
-        {
-            "base_dir": "results_pso_sweep_phi_do_feb26_daytime",
-            "tf_params": {
-                'tf_num': [2.430],
-                'tf_den': [3492.589, 1],
-                'tf_delay': 0.0,
-                'tf_n_pade': 2,
-                'computed_delay': 0.05,
-                'is_reverse_acting': False,
-                'max_kp': 100.0
-            }
+        "pso_sweep_phi_do_feb26_daytime": {
+            'tf_num': [2.430],
+            'tf_den': [3492.589, 1],
+            'tf_delay': 0.0,
+            'tf_n_pade': 2,
+            'computed_delay': 0.05,
+            'is_reverse_acting': False,
+            'max_kp': 100.0
         },
-        {
-            "base_dir": "results_pso_sweep_phi_do_feb5_nighttime",
-            "tf_params": {
-                'tf_num': [2.355],
-                'tf_den': [3083.590, 1],
-                'tf_delay': 0.0,
-                'tf_n_pade': 2,
-                'computed_delay': 0.05,
-                'is_reverse_acting': False,
-                'max_kp': 100.0
-            }
+        "pso_sweep_phi_do_feb5_nighttime": {
+            'tf_num': [2.355],
+            'tf_den': [3083.590, 1],
+            'tf_delay': 0.0,
+            'tf_n_pade': 2,
+            'computed_delay': 0.05,
+            'is_reverse_acting': False,
+            'max_kp': 100.0
         },
-        {
-            "base_dir": "results_pso_sweep_phi_do_feb7_nighttime",
-            "tf_params": {
-                'tf_num': [2.049],
-                'tf_den': [4499.996, 1],
-                'tf_delay': 0.0,
-                'tf_n_pade': 2,
-                'computed_delay': 0.05,
-                'is_reverse_acting': False,
-                'max_kp': 100.0
-            }
+        "pso_sweep_phi_do_feb7_nighttime": {
+            'tf_num': [2.049],
+            'tf_den': [4499.996, 1],
+            'tf_delay': 0.0,
+            'tf_n_pade': 2,
+            'computed_delay': 0.05,
+            'is_reverse_acting': False,
+            'max_kp': 100.0
         },
-        {
-            "base_dir": "results_pso_sweep_phi_do_feb25_nighttime",
-            "tf_params": {
-                'tf_num': [3.923],
-                'tf_den': [3012.232, 1],
-                'tf_delay': 0.0,
-                'tf_n_pade': 2,
-                'computed_delay': 0.05,
-                'is_reverse_acting': False,
-                'max_kp': 100.0
-            }
+        "pso_sweep_phi_do_feb25_nighttime": {
+            'tf_num': [3.923],
+            'tf_den': [3012.232, 1],
+            'tf_delay': 0.0,
+            'tf_n_pade': 2,
+            'computed_delay': 0.05,
+            'is_reverse_acting': False,
+            'max_kp': 100.0
         },
-        {
-            "base_dir": "results_pso_sweep_phi_do_feb26_nighttime",
-            "tf_params": {
-                'tf_num': [3.132],
-                'tf_den': [2530.052, 1],
-                'tf_delay': 0,
-                'tf_n_pade': 2,
-                'computed_delay': 0.05,
-                'is_reverse_acting': False,
-                'max_kp': 100.0
-            }
+        "pso_sweep_phi_do_feb26_nighttime": {
+            'tf_num': [3.132],
+            'tf_den': [2530.052, 1],
+            'tf_delay': 0,
+            'tf_n_pade': 2,
+            'computed_delay': 0.05,
+            'is_reverse_acting': False,
+            'max_kp': 100.0
         },
-        {
-            "base_dir": "results_pso_sweep_phi_tds_feb09_10",
-            "tf_params": {
-                'tf_num': [-21.082],
-                'tf_den': [71160.91, 1],
-                'tf_delay': 0,
-                'tf_n_pade': 2,
-                'computed_delay': 0.05,
-                'is_reverse_acting': True,
-                'max_kp': -100.0
-            }
+        "pso_sweep_phi_tds_feb09_10": {
+            'tf_num': [-21.082],
+            'tf_den': [71160.91, 1],
+            'tf_delay': 0,
+            'tf_n_pade': 2,
+            'computed_delay': 0.05,
+            'is_reverse_acting': True,
+            'max_kp': -100.0
         },
-        {
-            "base_dir": "results_pso_sweep_phi_tds_feb10_11",
-            "tf_params": {
-                'tf_num': [-15.519],
-                'tf_den': [40156.08, 1],
-                'tf_delay': 0,
-                'tf_n_pade': 2,
-                'computed_delay': 0.05,
-                'is_reverse_acting': True,
-                'max_kp': -100.0
-            }
+        "pso_sweep_phi_tds_feb10_11": {
+            'tf_num': [-15.519],
+            'tf_den': [40156.08, 1],
+            'tf_delay': 0,
+            'tf_n_pade': 2,
+            'computed_delay': 0.05,
+            'is_reverse_acting': True,
+            'max_kp': -100.0
         },
-        {
-            "base_dir": "results_pso_sweep_phi_tds_feb11_12",
-            "tf_params": {
-                'tf_num': [-12.458],
-                'tf_den': [16825.29, 1],
-                'tf_delay': 0,
-                'tf_n_pade': 2,
-                'computed_delay': 0.05,
-                'is_reverse_acting': True,
-                'max_kp': -100.0
-            }
+        "pso_sweep_phi_tds_feb11_12": {
+            'tf_num': [-12.458],
+            'tf_den': [16825.29, 1],
+            'tf_delay': 0,
+            'tf_n_pade': 2,
+            'computed_delay': 0.05,
+            'is_reverse_acting': True,
+            'max_kp': -100.0
         }
-    ]
+    }
 
     base_config = {
         'patience_limit': 25,
