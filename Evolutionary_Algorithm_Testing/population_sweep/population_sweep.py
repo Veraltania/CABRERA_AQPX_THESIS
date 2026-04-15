73f967c2cedc60fe681f5667bd1be315f9949520
Veralta5630 <neo.elrond.cabrera@gmail.com>
2026-04-15 22:48:54 +0900

    deleted garbage pop sweep data

--- Evolutionary_Algorithm_Testing/population_sweep/population_sweep.py
+++ Evolutionary_Algorithm_Testing/population_sweep/population_sweep.py
@@ -184,7 +184,7 @@ if __name__ == "__main__":
     pop_sizes = list(range(START_POP, END_POP + 1, STEP_SIZE))
     if pop_sizes[-1] != END_POP: pop_sizes.append(END_POP)
 
-    batch_dir = "BATCH_OPENLOOP_CONTROL_EFFORT_1X"
+    batch_dir = "BATCH_OPENLOOP_CONTROL_EFFORT"
     sweep_type = "population_sweep"
 
     # --- DEFINING TRANSFER FUNCTIONS WITH SPECIFIC Kp & Ki BOUNDS ---
